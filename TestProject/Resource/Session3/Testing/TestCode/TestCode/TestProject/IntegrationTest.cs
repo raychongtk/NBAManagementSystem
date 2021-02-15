8487d76faa9ead148ee3e442a7279715a12116b8
raychong <raychongtk@gmail.com>
2021-02-16 00:31:38 +0800

    add test project and resources

---
+++ TestProject/Resource/Session3/Testing/TestCode/TestCode/TestProject/IntegrationTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test2Code;
+
+namespace Test2Project
+{
+    /*
+     * You should finish the Integration test about the Validation class.
+     */
+
+    [TestClass]
+    public class IntegrationTest
+    {
+        //[TestMethod]
+        //public void DemoMethod()
+        //{
+        //  
+        //}
+
+
+    }
+}
---
+++ TestProject/Resource/Session3/Testing/TestCode/TestCode/TestProject/UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test2Code;
+
+namespace Test2Project
+{
+    /*
+     * You should finish the following tests:
+     * 1.Design 2 test cases to test the IsLeapYear method in the Validation class.
+     */
+
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void IsLeapYearMethod()
+        {
+
+        }
+
+    }
+}
